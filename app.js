978186e71ce1fadbbcce1ecc21a4b48eed00674e
Vijay11945 <120110419+Vijay11945@users.noreply.github.com>
2023-08-05 22:22:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/app-pro',(req, res, next) => {
+    console.log('start the middleware!');
+    res.send('<form action = "/pro" methos ="POST"> <input type = "text" name = "title"><button type = "submit">ADD</button></form>');
+    //next(); // Allows the request to continue to the next middleware in line
+});
+app.post('/pro',(req, res, next) => {
+    //console.log('In the middleware!');
+    //res.send('<h1>Hello from Vijay Pro!</h1>');
+    console.log(req.body);
+    res.redirect('/');
+    //next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.send( { key1: 500 })
+    //res.send( { key1: 500 })
     res.send('<h1>Hello from Vijay!</h1>');
    
 });
