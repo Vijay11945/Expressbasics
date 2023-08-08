b1a176a0c39354c895438f1013cfc0020a6bb1dc
Vijay11945 <120110419+Vijay11945@users.noreply.github.com>
2023-08-08 18:17:48 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const path = require('path');
 
+const rootDir = require('../util/path');
 const router = express.Router();
 
-router.get('/app-pro',(req, res, next) => {
-    console.log('start the middleware!');
-    res.send('<form action = "/admin/app-pro" method ="POST"> <input type = "text" name = "title"><button type = "submit">ADD</button></form>');
-    //next(); // Allows the request to continue to the next middleware in line
-});
 
+router.get('/app-pro',(req, res, next) => {
+    //console.log('start the middleware!');
+    //res.send('<form action = "/admin/app-pro" method ="POST"> <input type = "text" name = "title"><button type = "submit">ADD</button></form>');
+    //next(); // Allows the request to continue to the next middleware in line
+    res.sendFile(path.join(rootDir,'views','app-pro.html'));
+});
+ 
 router.post('/app-pro',(req, res, next) => {
-    console.log('In the middleware!');
+    //console.log('In the middleware!');
     //res.send('<h1>Hello from Vijay Pro!</h1>');
     console.log(req.body);
     res.redirect('/');
--- shop.js
+++ shop.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
     //console.log('In another middleware!');
     //res.send( { key1: 500 })
-    res.send('<h1>Hello from Vijay!</h1>');
-   
+   // res.send('<h1>Hello from Vijay!</h1>');
+   res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = router;
